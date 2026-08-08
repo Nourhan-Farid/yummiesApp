7e2c57a01dbfd32c85b02b5714abcd4614a8dbe1
NourhanFareed <nourhanfarid883@gmail.com>
2026-08-09 00:24:27 +0300

    3

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import App from './App.jsx'
-// import { BrowserRouter } from 'react-router-dom'
-
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/yummiesApp">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
