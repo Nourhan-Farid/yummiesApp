76f55e61fa99e20025d019c35c62bd50009c1d0f
NourhanFareed <nourhanfarid883@gmail.com>
2026-08-09 00:13:22 +0300

    2

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
